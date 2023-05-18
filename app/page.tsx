b2c2f236ead73036a7287794ccb3d7218ed59c4e
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-05-18 17:56:27 +0200

    Configure homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
-const Page = () => (
-    <main>
-      foo bar baz
-    </main>
-);
+import React from 'react';
+import container from '../config/container';
+import ContentLayout from '@/components/ContentLayout';
+import getMetadataGenerator from '@/helpers/metadata';
+import Content from '@/services/content/Content';
+import { Site } from '@/types/SiteConfig';
 
-export default Page;
+export const generateMetadata = getMetadataGenerator(undefined, '/');
+
+const Homepage = async () => {
+  const contentLoader = container.get<Content>('content');
+  const { title } = container.get<Site>('params.site_config');
+
+  const page = contentLoader.getPageWithFallback('/', contentLoader.getDummyPage(
+    'pages',
+    title,
+    '/',
+  ));
+
+  return (<ContentLayout page={ page } />);
+};
+
+export default Homepage;
