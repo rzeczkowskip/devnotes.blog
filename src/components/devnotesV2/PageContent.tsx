7c2fc0a38f8eed645cb3fa0816b1eac69e4f4207
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-10-15 08:04:28 +0200

    Create page content component

--- src/components/devnotesV2/PageContent.tsx
+++ src/components/devnotesV2/PageContent.tsx
@@ -1,13 +1,43 @@
 import React, { PropsWithChildren } from 'react';
-import { Page } from '@/types/Content';
+import { ContentItem, Page } from '@/types/Content';
 
 type PageContentProps = PropsWithChildren<{
   page: Page;
 }>;
 
+enum PageType {
+  Content = 'content',
+  ContentList = 'contentList',
+  TaxonomiesList = 'taxonomies',
+}
+
+const getPageType = (contentItem: ContentItem): PageType => {
+  if (contentItem.list?.collection) {
+    return contentItem.list?.isTaxonomyList
+      ? PageType.TaxonomiesList
+      : PageType.ContentList;
+  }
+
+  return PageType.Content;
+};
+
 const PageContent: React.FC<PageContentProps> = ({ page }) => {
-  console.log(page);
-  return <div></div>;
+  const { contentItem, relatedItems, listItems, taxonomies } = page;
+  const pageType = getPageType(contentItem);
+
+  if (pageType === PageType.ContentList) {
+    return <div></div>;
+  }
+
+  if (pageType === PageType.TaxonomiesList) {
+    return <div></div>;
+  }
+
+  if (pageType === PageType.Content) {
+    return <div></div>;
+  }
+
+  throw new Error(`Unsupported page type ${pageType}`);
 };
 
 export default PageContent;
