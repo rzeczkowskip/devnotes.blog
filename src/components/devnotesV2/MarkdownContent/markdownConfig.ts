4ac66d0da18970b39461269808331cb623926bd9
Piotr Rzeczkowski <rzeczkowski.piotr@gmail.com>
2023-10-25 23:23:45 +0200

    Fix plugin types

--- src/components/devnotesV2/MarkdownContent/markdownConfig.ts
+++ src/components/devnotesV2/MarkdownContent/markdownConfig.ts
@@ -1,15 +1,16 @@
 import hljsPowershell from 'highlight.js/lib/languages/powershell';
 import hljsTwig from 'highlight.js/lib/languages/twig';
+import { ReactMarkdownOptions } from 'react-markdown/lib/react-markdown';
 import rehypeExternalLinks, {
   Options as ExternalLinksOptions,
 } from 'rehype-external-links';
 import rehypeHighlight, { Options as HighlightOptions } from 'rehype-highlight';
 import remarkGfm from 'remark-gfm';
 import remarkUnwrapImages from 'remark-unwrap-images';
-import { PluggableList } from 'unified';
 import rehypeCallout from '@/components/devnotesV2/MarkdownContent/rehype/rehype-callout';
 
-export const rehypePlugins: PluggableList = [
+export const rehypePlugins: ReactMarkdownOptions['rehypePlugins'] = [
+  // @ts-ignore
   rehypeCallout,
   [
     rehypeExternalLinks,
@@ -29,4 +30,7 @@ export const rehypePlugins: PluggableList = [
   ],
 ];
 
-export const remarkPlugins: PluggableList = [remarkGfm, remarkUnwrapImages];
+export const remarkPlugins: ReactMarkdownOptions['remarkPlugins'] = [
+  remarkGfm,
+  remarkUnwrapImages,
+];
